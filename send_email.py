28c5ab2b158bf97debd6420e8ae5209f187ac492
Deon Jordaan <deonhjordaan@gmail.com>
2023-08-20 14:03:44 +0200

    remove password

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message):
@@ -7,7 +8,7 @@ def send_email(message):
     port = 465
 
     username = "deonhjordaan@gmail.com"
-    password = "qesniotliuomklwx"
+    password = os.getenv("EMAILPASS")
 
     receiver = "deonhjordaan@gmail.com"
     context = ssl.create_default_context()
